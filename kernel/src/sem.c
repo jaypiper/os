d74d009b09f6223adeb8c5d59a843146271f2350
Chen Lu <181250012@smail.nju.edu.cn>
2022-03-07 10:03:52 +0800

    sem: fix wait/signal

--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -13,24 +13,26 @@ void sem_init(sem_t *sem, const char *name, int value){
 void sem_wait(sem_t *sem){
   Assert(CHECK_SEM(sem), "sem fence");
   spin_lock(&sem->lock);
-  Assert(sem->count >= 0, "count of sem (%s) is negative (%d)", sem->name, sem->count);
-  while(sem->count == 0){
-    mark_not_runable(sem, cpu_current());
-    spin_unlock(&sem->lock);
-    yield();
-    spin_lock(&sem->lock);
-  }
   sem->count --;
+  int is_blocked;
+  if(sem->count < 0){
+    mark_not_runable(sem, cpu_current());
+    is_blocked = 1;
+  }
   spin_unlock(&sem->lock);
+  if(is_blocked){
+    Assert(ienabled(), "interrupt is disabled in sem wait %s", sem->name);
+    yield();
+  }
   Assert(CHECK_SEM(sem), "sem fence");
 }
 
 void sem_signal(sem_t *sem){
   Assert(CHECK_SEM(sem), "sem fence");
   spin_lock(&sem->lock);
-  Assert(sem->count >= 0, "count of sem (%s) is negative (%d)", sem->name, sem->count);
-  if(sem->wait_list) wakeup_task(sem);
+  Assert(sem->count >= 0 || sem->wait_list, "sem %s with count %d has empty wait_list", sem->name, sem->count);
   sem->count ++;
+  if(sem->wait_list) wakeup_task(sem);
   spin_unlock(&sem->lock);
   Assert(CHECK_SEM(sem), "sem fence");
 }
