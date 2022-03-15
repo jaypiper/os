52d9e83ca248060553b9e342bf235346ebaa3ef9
Chen Lu <181250012@smail.nju.edu.cn>
2022-03-16 00:45:36 +0800

    debug: fix Assert for mkfs

--- kernel/include/debug.h
+++ kernel/include/debug.h
@@ -5,17 +5,29 @@
     printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
+#ifdef MKFS_DEBUG
 #define Assert(cond, ...) \
   do { \
     if (!(cond)) { \
-      iset(false); \
       printf("\33[1;31m"); \
       printf(__VA_ARGS__); \
       printf("\33[0m\n"); \
       assert(cond); \
     } \
   } while (0)
-
+#else
+#define Assert(cond, ...) \
+  do { \
+    if (!(cond)) { \
+      iset(false); \
+      printf("\33[1;31m [%d]", cpu_current()); \
+      printf(__VA_ARGS__); \
+      printf("\33[0m\n"); \
+      assert(cond); \
+      halt(-1); \
+    } \
+  } while (0)
+#endif
 #define os_panic(...) Assert(0, __VA_ARGS__)
 
 #define TODO() os_panic("please implement me")
