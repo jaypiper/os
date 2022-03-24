66d5e059cd40011fa232ceeb2e8f91f8b15e8bf5
Chen Lu <181250012@smail.nju.edu.cn>
2022-03-24 17:25:51 +0800

    vfs: update inode.size in file_write

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -391,6 +391,8 @@ static int file_write(ofile_info_t* ofile, int fd, void *buf, int count){
 		blk_offset = 0;
 		inode_blk_idx ++;
 	}
+	inode.size = ofile->offset + count;
+	sd_write(INODE_ADDR(ofile->inode_no) + OFFSET_IN_STRUCT(inode, size), &inode.size, sizeof(int));
 	ofile->offset += count;
 	return count;
 }
