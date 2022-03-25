8f1c4fc6584e4f3a209b50ed0b314260aa74b410
Chen Lu <181250012@smail.nju.edu.cn>
2022-03-25 15:24:34 +0800

    vfs,open: fix open for link file

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -599,6 +599,10 @@ static int vfs_open(const char *pathname, int flags){  // must start with /
 		printf("open: no such file or directory %s\n", pathname);
 		return -1;
 	}
+	while(file_inode.type == FT_LINK) {
+		file_inode_no = link_inodeno_by_inode(&file_inode);
+		get_inode_by_no(file_inode_no, &file_inode);
+	}
 	ofile_info_t* tmp_ofile = pmm->alloc(sizeof(ofile_info_t));
 	tmp_ofile->write = (flags & O_WRONLY) || (flags & O_RDWR) ? file_write : invalid_write;
 	tmp_ofile->read = (flags & O_RDONLY) || (flags & O_RDWR) ? file_read : invalid_write;
