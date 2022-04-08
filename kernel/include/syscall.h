fc1c28af821b9b696de8a5ed7981fbed6f852464
Chen Lu <181250012@smail.nju.edu.cn>
2022-04-08 11:16:23 +0800

    syscall: add more syscall numbers

--- kernel/include/syscall.h
+++ kernel/include/syscall.h
@@ -1,21 +1,26 @@
 #ifndef OS_SYSCALL_H
 #define OS_SYSCALL_H
 
-#define SYS_READ      0x0
-#define SYS_WRITE     0x1
-#define SYS_OPEN      0x2
-#define SYS_CLOSE     0x3
-#define SYS_FSTAT     0x5
-#define SYS_LSEEK     0x8
-#define SYS_MMAP      0x9
-#define SYS_DUP       0x20
-#define SYS_FORK      0x39
-#define SYS_EXECVE    0x3b
-#define SYS_EXIT      0x3c
-#define SYS_CHDIR     0x50
-#define SYS_MKDIR     0x53
-#define SYS_LINK      0x56
-#define SYS_UNLINK    0x57
+#define SYS_READ          0x0
+#define SYS_WRITE         0x1
+#define SYS_OPEN          0x2
+#define SYS_CLOSE         0x3
+#define SYS_FSTAT         0x5
+#define SYS_LSEEK         0x8
+#define SYS_MMAP          0x9
+#define SYS_BRK           0xc
+#define SYS_GETPID        0x14
+#define SYS_YIELD         0x18
+#define SYS_DUP           0x20
+#define SYS_FORK          0x39
+#define SYS_EXECVE        0x3b
+#define SYS_EXIT          0x3c
+#define SYS_KILL          0x3e
+#define SYS_CHDIR         0x50
+#define SYS_MKDIR         0x53
+#define SYS_LINK          0x56
+#define SYS_UNLINK        0x57
+#define SYS_GETTIMEOFDAY  0x60
 
 #define MAX_SYSCALL_IDX 0x100
 
