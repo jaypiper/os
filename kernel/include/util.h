97662adee7fdc1682bd74ab5aa7f01288c3b660a
Chen Lu <181250012@smail.nju.edu.cn>
2022-05-28 20:01:36 +0800

    util: add STRUCT_OFFSET

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -7,4 +7,6 @@ int find_replace(char* s, char* delim, int start_pos);
 #define MAX(a, b) (((a) > (b))? (a) : (b))
 #define MIN(a, b) (((a) > (b))? (b) : (a))
 
+#define STRUCT_OFFSET(TYPE, MEMBER) ((uintptr_t)(&(((TYPE *)0)->MEMBER)))
+
 #endif
