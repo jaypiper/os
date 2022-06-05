410aefae89c5219ac95cb852a02387b8ff714859
Chen Lu <181250012@smail.nju.edu.cn>
2022-06-05 10:26:02 +0800

    fat: fix wc width

--- kernel/src/fat.c
+++ kernel/src/fat.c
@@ -408,7 +408,7 @@ static void insert_dirent_to_dirent(dirent_t *parent, dirent_t* child, uint32_t 
     for(int i = entryNum; i > 0; i--){  // long name
       fentry.ld.Ord = i | (i == entryNum ? LAST_LONG_ENTRY : 0);
       uint8_t* str = child->name + (i - 1) * LONG_NAME_LENGTH;
-      uint8_t wc[LONG_NAME_LENGTH];
+      uint8_t wc[LONG_NAME_LENGTH * 2];
       c2wc(wc, str, LONG_NAME_LENGTH);
       memmove(fentry.ld.name1, wc, 10);
       memmove(fentry.ld.name2, wc + 10, 12);
