179568c0893299ce8d2ef3a022edd761f110f8bd
Chen Lu <181250012@smail.nju.edu.cn>
2022-08-01 11:16:59 +0800

    trm: adapt putch to qemu

--- kernel/src/trm.c
+++ kernel/src/trm.c
@@ -17,7 +17,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
-  uarths_putchar(ch);
+  drv_uart_putc(ch);
 }
 
 void halt(int code) {
