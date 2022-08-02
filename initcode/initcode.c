d0cdb576d0f7c5a7e72cc135292ee8befc61081e
Chen Lu <181250012@smail.nju.edu.cn>
2022-08-02 15:58:15 +0800

    refactor initcode.c

--- initcode/initcode.c
+++ initcode/initcode.c
@@ -1,122 +1,37 @@
 #include <syscall.h>
 /* The first program in user mode */
 
-char oscmp_static_str[][35] = {
-"argv",
-"basename",
-"clocale_mbfuncs",
-"clock_gettime",
-"crypt",
-"dirname",
-"env",
-"fdopen",
-"fnmatch",
-"fscanf",
-"fwscanf",
-"iconv_open",
-"inet_pton",
-"mbc",
-"memstream",
-"pthread_cancel_points",
-"pthread_cancel",
-"pthread_cond",
-"pthread_tsd",
-"qsort",
-"random",
-"search_hsearch",
-"search_insque",
-"search_lsearch",
-"search_tsearch",
-"setjmp",
-"snprintf",
-"socket",
-"sscanf",
-"sscanf_long",
-"stat",
-"strftime",
-"string",
-"string_memcpy",
-"string_memmem",
-"string_memset",
-"string_strchr",
-"string_strcspn",
-"string_strstr",
-"strptime",
-"strtod",
-"strtod_simple",
-"strtof",
-"strtol",
-"strtold",
-"swprintf",
-"tgmath",
-"time",
-"tls_align",
-"udiv",
-"ungetc",
-"utime",
-"wcsstr",
-"wcstol",
-"pleval",
-"daemon_failure",
-"dn_expand_empty",
-"dn_expand_ptr_0",
-"fflush_exit",
-"fgets_eof",
-"fgetwc_buffering",
-"flockfile_list",
-"fpclassify_invalid_ld80",
-"ftello_unflushed_append",
-"getpwnam_r_crash",
-"getpwnam_r_errno",
-"iconv_roundtrips",
-"inet_ntop_v4mapped",
-"inet_pton_empty_last_field",
-"iswspace_null",
-"lrand48_signextend",
-"lseek_large",
-"malloc_0",
-"mbsrtowcs_overflow",
-"memmem_oob_read",
-"memmem_oob",
-"mkdtemp_failure",
-"mkstemp_failure",
-"printf_1e9_oob",
-"printf_fmt_g_round",
-"printf_fmt_g_zeros",
-"printf_fmt_n",
-"pthread_robust_detach",
-"pthread_cancel_sem_wait",
-"pthread_cond_smasher",
-"pthread_condattr_setclock",
-"pthread_exit_cancel",
-"pthread_once_deadlock",
-"pthread_rwlock_ebusy",
-"putenv_doublefree",
-"regex_backref_0",
-"regex_bracket_icase",
-"regex_ere_backref",
-"regex_escaped_high_byte",
-"regex_negated_range",
-"regexec_nosub",
-"rewind_clear_error",
-"rlimit_open_files",
-"scanf_bytes_consumed",
-"scanf_match_literal_eof",
-"scanf_nullbyte_char",
-"setvbuf_unget",
-"sigprocmask_internal",
-"sscanf_eof",
-"statvfs",
-"strverscmp",
-"syscall_sign_extend",
-"uselocale_0",
-"wcsncpy_read_overflow",
-"wcsstr_false_negative",
-};
+#define TESTS_STATIC(f) \
+f("argv")    f("basename")    f("clocale_mbfuncs") f("crypt")     f("string_memcpy") \
+f("fnmatch") f("fwscanf")     f("iconv_open")      f("env")       f("inet_pton") \
+f("mbc")     f("memstream")   f("clock_gettime")   f("setjmp")    f("socket") \
+f("dirname") f("qsort")       f("random")          f("snprintf")  f("sscanf") \
+f("stat")    f("sscanf_long") f("wcstol")          f("pleval")    f("daemon_failure")\
+f("string")  f("strftime")    f("string_memmem")   f("string_memset") f("string_strchr") \
+f("strtof")  f("strtol")      f("strptime")        f("strtod")    f("strtod_simple") \
+f("fscanf")  f("strtold")     f("swprintf")        f("tgmath")    f("tls_align") \
+f("time")    f("udiv")        f("ungetc")          f("utime")     f("wcsstr") \
+f("fdopen")  f("malloc_0")    f("iswspace_null")   f("lseek_large")f("lrand48_signextend") \
+f("search_hsearch")  f("search_insque") f("search_lsearch")   f("search_tsearch") \
+f("string_strcspn")  f("string_strstr") f("dn_expand_empty")  f("dn_expand_ptr_0") \
+f("fflush_exit")     f("fgets_eof")     f("fgetwc_buffering") f("flockfile_list") \
+f("fpclassify_invalid_ld80") f("ftello_unflushed_append") f("getpwnam_r_crash") \
+f("pthread_cancel_points") f("pthread_cancel") f("pthread_cond") f("pthread_tsd") \
+f("getpwnam_r_errno")    f("iconv_roundtrips") f("inet_ntop_v4mapped") \
+f("inet_pton_empty_last_field") f("mbsrtowcs_overflow") \
+f("memmem_oob_read") f("memmem_oob") f("mkdtemp_failure") f("mkstemp_failure") \
+f("printf_1e9_oob") f("printf_fmt_g_round") f("printf_fmt_g_zeros") f("printf_fmt_n") \
+f("pthread_robust_detach") f("pthread_cancel_sem_wait") f("pthread_cond_smasher") \
+f("pthread_condattr_setclock") f("pthread_exit_cancel") f("pthread_once_deadlock") \
+f("pthread_rwlock_ebusy") f("putenv_doublefree") f("regex_backref_0") \
+f("regex_bracket_icase") f("regex_ere_backref") f("regex_escaped_high_byte") \
+f("regex_negated_range") f("regexec_nosub") f("rewind_clear_error") \
+f("rlimit_open_files") f("scanf_bytes_consumed") f("scanf_match_literal_eof") \
+f("scanf_nullbyte_char") f("setvbuf_unget") f("sigprocmask_internal") f("sscanf_eof") \
+f("statvfs") f("strverscmp") f("syscall_sign_extend") \
+f("uselocale_0") f("wcsncpy_read_overflow") f("wcsstr_false_negative")
 
-
-
-char initcode_str[][25] = {
+char initcode_str[][32] = {
   "in initcode\n",
   "./runtest.exe",
   "-w",
@@ -136,14 +51,18 @@ int initcode_syscall(int syscall, unsigned long long val1, unsigned long long va
   return ret;
 }
 
-char* initcode_args[] = {
-  initcode_str[1], initcode_str[2], initcode_str[3], oscmp_static_str[0], 0
+#define OSCMP_TEST_STATIC(_) {initcode_str[1], initcode_str[2], initcode_str[3], _, 0},
+
+char* initcode_args[][5] = {
+  TESTS_STATIC(OSCMP_TEST_STATIC)
 };
 
 void oscmp_test_static(int i){
   int pid = initcode_syscall(SYS_clone, 17, 0, 0);
   if(pid == 0){
-    initcode_syscall(SYS_execve, initcode_str[1], initcode_args, 0);
+    initcode_syscall(SYS_execve, initcode_str[1], initcode_args[i], 0);
+  } else{
+    initcode_syscall(SYS_wait4, 0, 0, 0);
   }
 
 }
@@ -151,6 +70,8 @@ void oscmp_test_static(int i){
 void __attribute__((section("initcode_entry"))) initcode_test(){
   initcode_syscall(SYS_write, 1, initcode_str[0], 12);
   // Testing busybox
-  oscmp_test_static(0);
+  for(int i = 0; i < 61; i++)
+  oscmp_test_static(i);
+  // initcode_syscall(SYS_write, 1, "\ngoodbye\n", 8);
   while(1);
 }
