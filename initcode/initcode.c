25d114a55179e48e83daa5bbcb2e140345220aa5
Chen Lu <181250012@smail.nju.edu.cn>
2022-08-05 11:06:30 +0800

    initcode: add busybox tests

--- initcode/initcode.c
+++ initcode/initcode.c
@@ -31,12 +31,71 @@ f("scanf_nullbyte_char") f("setvbuf_unget") f("sigprocmask_internal") f("sscanf_
 f("statvfs") f("strverscmp") f("syscall_sign_extend") \
 f("uselocale_0") f("wcsncpy_read_overflow") f("wcsstr_false_negative")
 
+#define TESTS_BUSYBOX(f) \
+f("echo \"#### independent command test\"") \
+f("ash -c exit") \
+f("sh -c exit") \
+f("basename /aaa/bbb") \
+f("cal") \
+f("clear") \
+f("date ") \
+f("df ") \
+f("dirname /aaa/bbb") \
+f("dmesg ") \
+f("du") \
+f("expr 1 + 1") \
+f("false") \
+f("true") \
+f("which ls") \
+f("uname") \
+f("uptime") \
+f("printf \"abc\n\"") \
+f("ps") \
+f("pwd") \
+f("free") \
+f("hwclock") \
+f("kill 10") \
+f("ls") \
+f("sleep 1") \
+f("echo \"#### file opration test\"") \
+f("touch test.txt") \
+f("echo \"hello world\" > test.txt") \
+f("cat test.txt") \
+f("cut -c 3 test.txt") \
+f("od test.txt") \
+f("head test.txt") \
+f("tail test.txt ") \
+f("hexdump -C test.txt ") \
+f("md5sum test.txt") \
+f("echo \"ccccccc\" >> test.txt") \
+f("echo \"bbbbbbb\" >> test.txt") \
+f("echo \"aaaaaaa\" >> test.txt") \
+f("echo \"2222222\" >> test.txt") \
+f("echo \"1111111\" >> test.txt") \
+f("echo \"bbbbbbb\" >> test.txt") \
+f("sort test.txt | ./busybox uniq") \
+f("stat test.txt") \
+f("strings test.txt ") \
+f("wc test.txt") \
+f("[ -f test.txt ]") \
+f("more test.txt") \
+f("rm test.txt") \
+f("mkdir test_dir") \
+f("mv test_dir test") \
+f("rmdir test") \
+f("grep hello busybox_cmd.txt") \
+f("cp busybox_cmd.txt busybox_cmd.bak") \
+f("rm busybox_cmd.bak") \
+f("find -name \"busybox_cmd.txt\"") \
+
+
 char initcode_str[][32] = {
   "in initcode\n",
   "./runtest.exe",
   "-w",
   "entry-static.exe",
   "./hello",
+  "./busybox"
 };
 
 
@@ -52,9 +111,14 @@ int initcode_syscall(int syscall, unsigned long long val1, unsigned long long va
 }
 
 #define OSCMP_TEST_STATIC(_) {initcode_str[1], initcode_str[2], initcode_str[3], _, 0},
+#define OSCMP_TEST_BUSYBOX(_) {initcode_str[5], _, 0},
 
 char* initcode_args[][5] = {
   TESTS_STATIC(OSCMP_TEST_STATIC)
+};
+
+char* busybox_cmd[][3] = {
+  TESTS_BUSYBOX(OSCMP_TEST_BUSYBOX)
 };
 
 void oscmp_test_static(int i){
@@ -64,14 +128,28 @@ void oscmp_test_static(int i){
   } else{
     initcode_syscall(SYS_wait4, 0, 0, 0);
   }
+}
 
+
+void oscmp_test_busybox(int i){
+  int pid = initcode_syscall(SYS_clone, 17, 0, 0);
+  if(pid == 0){
+    initcode_syscall(SYS_execve, initcode_str[5], busybox_cmd[i], 0);
+  } else{
+    initcode_syscall(SYS_wait4, 0, 0, 0);
+  }
 }
 
 void __attribute__((section("initcode_entry"))) initcode_test(){
   initcode_syscall(SYS_write, 1, initcode_str[0], 12);
-  // Testing busybox
-  for(int i = 0; i < 61; i++)
-  oscmp_test_static(i);
+  // Testing libc-test
+  // for(int i = 0; i < 61; i++)
+  // oscmp_test_static(i);
+
+  // busybox
+  for(int i = 0; i < sizeof(busybox_cmd) / sizeof(busybox_cmd[0]); i++){
+    oscmp_test_busybox(i);
+  }
   // initcode_syscall(SYS_write, 1, "\ngoodbye\n", 8);
   while(1);
 }
