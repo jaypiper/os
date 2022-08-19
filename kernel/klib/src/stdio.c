5d0515693f9be4416e1bb768f65d6cf724499d05
Chen Lu <181250012@smail.nju.edu.cn>
2022-08-19 21:59:52 +0800

    klib: set printf buffer to 256

--- kernel/klib/src/stdio.c
+++ kernel/klib/src/stdio.c
@@ -23,7 +23,7 @@ static char* _fill(char* out, char ch, int len){
 int printf(const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
-  char buf[2048];
+  char buf[256];
   // memset(buf, 0, sizeof(buf));
   vsprintf(buf, fmt, args);
   putstr(buf);
